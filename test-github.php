89ca963c5e1fc2633264b82ea1c74f23f67395c0
eugenlisov <eugen.lisov@gmail.com>
2017-10-31 13:44:40 +0200

    Git Second commit

--- test-github.php
+++ test-github.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: DM Test Github Plugin
 Description: something...
-Version:     1.0.0.0
+Version:     1.0.0.1
 Author:      Eugen Lisov
 Author URI:  http://www.devmaverick.com/
 GitHub Plugin URI: eugenlisov/dm-test-github
@@ -12,7 +12,7 @@ GitHub Plugin URI: https://github.com/eugenlisov/dm-test-github
 *
 
 function create_something() {
-  echo 'Create something';
+  echo 'Nope, the function needs to so something else now';
 }
 
 ?>
