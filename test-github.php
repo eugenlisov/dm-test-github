5f0e8c5cceaa24abdda567e178ecd36307f13527
eugenlisov <eugen.lisov@gmail.com>
2017-10-31 13:37:16 +0200

    First Commit

---
+++ test-github.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: DM Test Github Plugin
+Description: something...
+Version:     1.0.0.0
+Author:      Eugen Lisov
+Author URI:  http://www.devmaverick.com/
+GitHub Plugin URI: eugenlisov/dm-test-github
+GitHub Plugin URI: https://github.com/eugenlisov/dm-test-github
+
+*/
+
+?>
