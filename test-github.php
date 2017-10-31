c298485aac79e83e5ca3776caf5929e43c760329
eugenlisov <eugen.lisov@gmail.com>
2017-10-31 13:49:46 +0200

    Updated Version and added proper description
    Also, the plugin now actually does something.

--- test-github.php
+++ test-github.php
@@ -1,8 +1,8 @@
 <?php
 /*
 Plugin Name: DM Test Github Plugin
-Description: something...
-Version:     1.0.0.1
+Description: Acum am pus o descriere de oameni civilizati
+Version:     1.0.0.2
 Author:      Eugen Lisov
 Author URI:  http://www.devmaverick.com/
 GitHub Plugin URI: eugenlisov/dm-test-github
@@ -14,5 +14,8 @@ GitHub Plugin URI: https://github.com/eugenlisov/dm-test-github
 function create_something() {
   echo 'Nope, the function needs to so something else now';
 }
+create_something();
+
+// Sugiuc
 
 ?>
